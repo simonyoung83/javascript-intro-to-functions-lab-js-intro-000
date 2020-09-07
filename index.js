303e10e8c89a1cdb86f2c8857a52c6c973cd20a6
Simon Young <simon.young83@me.com>
2020-09-07 20:44:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function logShout (string) {console.log (string. toUpperCase())}
 
 function logWhisper (string) {console.log (string. toLowerCase())}
 
-function sayHiToGrandma (string) {if (string. toLowerCase()=== string){ 
+function sayHiToGrandma (string) {if (string. toLowerCase()=== string){
   return "I can't hear you!"} {if (string. toUpperCase()=== string){
     return "YES INDEED"}
   }
