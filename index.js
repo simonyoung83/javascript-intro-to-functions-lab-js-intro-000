9b762ff3fc6ee9189d55ce8a079ab71df245ae17
Simon Young <simon.young83@me.com>
2020-09-07 17:49:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ function whisper (string) {return string. toLowerCase()
 }
 function logShout (string) {console.log (string. toUpperCase())}
 
-function logShout (string) {console.log (string. toLowerCase())}
+function logWhisper (string) {console.log (string. toLowerCase())}
